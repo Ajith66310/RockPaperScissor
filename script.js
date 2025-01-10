a4a7115a94d379b8b2f75964a9e7d0c36c9a6d44
Ajith k v <114228838+Ajith66310@users.noreply.github.com>
2025-01-11 02:01:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,11 +98,11 @@ function computerPlay() {
   let computerMove = "";
   const randomNum = Math.random();
   if (randomNum >= 0 && randomNum < 1 / 3) {
-    computerMove = "Rock";
+    computerMove = "rock";
   } else if (randomNum >= 1 / 3 && randomNum < 2 / 3) {
-    computerMove = "Paper";
+    computerMove = "paper";
   } else if (randomNum >= 2 / 3 && randomNum <= 1) {
-    computerMove = "Scissor";
+    computerMove = "scissor";
   }
   return computerMove;
 }
