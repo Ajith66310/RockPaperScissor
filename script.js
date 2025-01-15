83ff4f2417b0da9e65c4b47aff29ce32d0ce636e
Ajith k v <114228838+Ajith66310@users.noreply.github.com>
2025-01-15 14:13:27 +0530

    fixed image prblm(3)

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ function playerMoved(playerMove) {
   document.querySelector(".js-result").innerHTML = `${result}`;
 
   document.querySelector(".js-moves").innerHTML = `You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">  
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
+  <img src="${playerMove}-emoji.png" class="move-icon">  
+  <img src="${computerMove}-emoji.png" class="move-icon">
   Computer`;
 }
 function updateScoreElement() {
